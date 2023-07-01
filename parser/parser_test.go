9d465db76a018398f1260fa243437716b0e59949
Rockwell Schrock <rockwell@schrock.me>
2023-07-01 09:43:55 -0400

    2.6 - parse integer literals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -137,6 +137,33 @@ func TestIdentifierExpression(t *testing.T) {
 	}
 }
 
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+	program := parseProgram(t, input)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+
+	if ident.Value != 5 {
+		t.Fatalf("ident.Value is not %d. got=%d", 5, ident.Value)
+	}
+
+	if ident.TokenLiteral() != "5" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
+
 func parseProgram(t *testing.T, input string) *ast.Program {
 	l := lexer.New(input)
 	p := New(l)
